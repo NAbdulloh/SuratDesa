b32384d35efbbbd05aa4861d47fffb6b8f0b7f0f
NAbudulloh <16523112@students.uii.ac.id>
2019-08-13 10:20:26 +0700

    Detail Data

--- application/views/halamanUtama.php
+++ application/views/halamanUtama.php
@@ -3,12 +3,11 @@
 <head>
 	<title>Form Pengurusan Surat</title>
 	<link rel="stylesheet" href="bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 
 	<div class="container">
-					<form class="align-self-center">
+		<form class="col align-self-center">
             <fieldset>
             	<h4>Form Pengurusan Surat</h4>
                             <div class="form-group">
@@ -27,13 +26,90 @@
                                 <input type="text" name="nama" class="nama form-control" id="nama" required>
                             </div>
                             <br>
-                            <button type="button" class="btn btn-next btn-primary">Berikutnya <i class="fa fa-angle-right"></i></button>
+                            <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalLong">Berikutnya <i class="fa fa-angle-right"></i></button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Detail Data</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <h5>Hasil Input</h5>
+                                        <hr>
+                                        <div class="row">
+                                            <div class="col-sm-5">
+                                                <b>Nama Lengkap</b>
+                                            </div>
+                                            <div class="col-sm-7">
+                                                : 
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-5">
+                                                <b>No. NIK</b>
+                                            </div>
+                                            <div class="col-sm-7">
+                                                : 
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-5">
+                                                <b>Alamat</b>
+                                            </div>
+                                            <div class="col-sm-7">
+                                                : 
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-5">
+                                                <b>Tempat dan Tanggal Lahir</b>
+                                            </div>
+                                            <div class="col-sm-7">
+                                                : 
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-5">
+                                                <b>Usia</b>
+                                            </div>
+                                            <div class="col-sm-7">
+                                                : 
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-5">
+                                                <b>Pekerjaan</b>
+                                            </div>
+                                            <div class="col-sm-7">
+                                                : 
+                                            </div>
+                                        </div>
+
+                                        <hr>
+
+                                        <h5>Data Benar?</h5>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
+                                        <button type="button" class="btn btn-primary" data-dismiss="modal">Iya</button>
+                                        
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
             </fieldset>
-			    </form>
-			
-		</table>
+		</form>
 	
 	</div>
 
 </body>
+<script src="bootstrap.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
