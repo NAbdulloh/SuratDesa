5b9cff01a3280824c092c273cf0e88dce906e819
NAbudulloh <16523112@students.uii.ac.id>
2019-08-18 15:15:35 +0700

    terbaru

--- application/views/statusSurat.php
+++ application/views/statusSurat.php
@@ -1,9 +1,76 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Ini List Surat</title>
+    <title>Form Pengurusan Surat</title>
+    <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.css">
 </head>
 <body>
 
+    <div class="container">
+        <form class="col align-self-center">
+            <fieldset>
+    <div class="container">
+    <div class="jumbotron" style="background-color:#000; border: solid #000; opacity:0.8; filter:alpha(opacity=40);">
+        <div class="panel-body" style="background-color:#FFFFFF">
+            <table style="margin: auto;">
+                <td>
+                <center>
+                    <font color="#000000" size="2">
+                    <b>
+                        <h3>Pesanan Surat</h3>
+                    </b>
+                </center>
+                <br>
+                <center>
+                <table class="table table-striped" align="center">
+                    <tr>
+                        <td align="center"><b>ID Surat</b></td>
+                        <td align="center"><b>Jenis Surat</b></td>
+                        <td align="center"><b>NIK</b></td>
+                        <td align="center"><b>Nama Pemesan</b></td>
+                        <td align="center"><b>Status</b></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td align="center"><b>1</b></td>
+                        <td align="center"><b>SKCK</b></td>
+                        <td align="center"><b>12345678910</b></td>
+                        <td align="center"><b>Steven Paidi</b></td>
+                        <td align="center"><b>Sudah Jadi</b></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td align="center"><b>2</b></td>
+                        <td align="center"><b>SKCK</b></td>
+                        <td align="center"><b>12345678910</b></td>
+                        <td align="center"><b>Steven Paidu</b></td>
+                        <td align="center"><b>Sedang Diproses</b></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td align="center"><b>3</b></td>
+                        <td align="center"><b>SKCK</b></td>
+                        <td align="center"><b>12345678910</b></td>
+                        <td align="center"><b>Steven Paido</b></td>
+                        <td align="center"><b>Sedang Diproses</b></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+        </center>
+            </font>
+                </table>
+            </fieldset>
+        </form>
+    
+    </div>
+
 </body>
+<script src="bootstrap.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
