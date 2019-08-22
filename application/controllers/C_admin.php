612047c598e12030626aaaca205aa90e8d07dee0
NAbudulloh <16523112@students.uii.ac.id>
2019-08-22 13:28:49 +0700

    apdet

--- application/controllers/C_admin.php
+++ application/controllers/C_admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_list extends CI_Controller {
+class C_admin extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,7 +18,8 @@ class C_list extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index() {
-		$this->load->view('statusSurat');
+	public function index()
+	{
+		$this->load->view('halamanAdmin');
 	}
 }
